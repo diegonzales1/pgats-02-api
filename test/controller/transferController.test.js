c0ab22cf32d42b02d2cc549d7a5bce7a42045f44
dogs <diego.andrade@gft.com>
2025-09-01 17:23:06 -0300

    Add primeiro teste

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../app');
+
+describe('Transfer Controller', () => {
+  describe('POST /transfer', () => {
+    it('Quando informo remetente e destinatario inexistente recebo 400', async () => {
+      const resposta = await request(app)
+      .post('/transfers')
+      .send({
+        "from": "teste1",
+        "to": "teste2",
+        "value": 50
+      });
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+    });
+  });
+});
